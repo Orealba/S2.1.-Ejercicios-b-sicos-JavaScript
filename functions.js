a22f3c61d5173acfbe30c53f102d74be1f453127
Orealba Soriano <orealbas@gmail.com>
2024-10-15 12:06:47 +0200

    started 1.2

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+//Arrow functions
 //Nivel 1
 //Ejercicio 1
 let add = (firstNumber, secondNumber) => {
@@ -44,4 +45,55 @@ function sayHi() {
 
   let sayHi = newPerson.greet();
   document.getElementById('hi').innerHTML = sayHi;
+}
+
+//Nivel 2
+//Ejercicio 4
+//Función de flecha dentro de un loop: Crea una función llamada
+//printNumbers que acepte un array de números y utilice un loop
+// for para imprimir cada número en la consola utilizando una función de flecha.
+
+let printNumbers = () => {
+  let arrayNumber = [1, 2, 3, 4];
+  let showNumbers = printNumbers(arrayNumber);
+  console.log(showNumbers);
+};
+
+function showNumbers(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    () => console.log(numbers[i]);
+
+    document.getElementById('showNumbers').innerHTML = showNumbers;
+  }
+  return;
+}
+//Nivel3
+//ejercicio5
+let greetings = () => {
+  let name = 'Mia';
+
+  console.log(`Hola ${name}, bienvenida`);
+
+  return `Hola ${name}, bienvenida`;
+};
+
+function showNote() {
+  let noteName = greetings();
+  document.getElementById('showNote').innerHTML = noteName;
+}
+function time() {
+  setTimeout(showNote, 3000);
+}
+
+
+//Ejercicio 1.2:
+//nivel 1
+//ejercicio 5
+
+
+function puedeConducir() {
+  let userAge = parseFloat(document.getElementById("userAge").value);
+  let answer = userAge >= 18 ? "Puedes conducir" : "No puedes conducir";
+  console.log(answer)
+  document.getElementById("showMe").innerHTML = answer
 }
